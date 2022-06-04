e8694687a3e3556c2be682b9617b1cb6bc2bc547
brianliu <hong86323@gmail.com>
2022-06-04 22:10:23 +0800

    Add Mark component test.

---
+++ src/components/Mark/Mark.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Mark from "./index";
+
+test("Mark component should be non-clickable when the mark is set.", () => {
+  render(
+    <Mark symbol={"o"} size={{ width: 100, height: 100 }} onClick={() => {}} />
+  );
+  const markElement = screen.getByRole("button");
+  expect(markElement).toBeDisabled();
+});
